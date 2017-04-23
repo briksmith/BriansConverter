7e2611d3214a9107deb8f72cc0cc6790db52196d
Your Name <briksmith@earthlink.net>
2017-04-23 18:44:04 -0500

    Init commit

---
+++ briansConverter/src/main/java/Controller/BriansConverterApplication.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriansConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriansConverterApplication.class, args);
+	}
+}
---
+++ briansConverter/src/main/java/utils/Consts.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Consts
+{
+	private Consts() {}
+	
+	public static final String convertFrom = "Units to Convert From";
+	public static final String convertTo = "Units to Convert To";
+}
